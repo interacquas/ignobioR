9304db3cbf7613e384dc5ce96458a7af6f58a235
interacquas <marcodantra@gmail.com>
2020-06-21 22:15:59 +0200

    @

--- R/virtual_list.R
+++ R/virtual_list.R
@@ -18,7 +18,8 @@
 #' data(exclareas)
 #' 
 #' virtual_list(data_flor=datashort, site=site, excl_areas=exclareas, tau=30, upperlimit=25)}
-
+#' See more about the markdown markup at the
+#' [Commonmark web site](http://commonmark.org/help)
 
 virtual_list<-function(data_flor,site, year_study=NULL, excl_areas=NULL, CRS.new = 3035, tau, upperlimit=20){
 
